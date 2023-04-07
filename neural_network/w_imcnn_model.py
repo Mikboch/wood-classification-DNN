10a6f97c56aaaa03e447d84104c778e638c17e83
justleon <maniak656@gmail.com>
2023-04-07 16:15:36 +0200

    Basic W_IMCNN-Block and -Model implementation

---
+++ neural_network/w_imcnn_model.py
@@ -0,0 +1,43 @@
+from w_imcnn_block import WIMCNNBlock
+from torch import nn
+
+
+class WIMCNNModel(nn.Module):
+    def __init__(self, num_classes=16, in_channels=3):
+        super().__init__()
+
+        self.convBlock1 = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, stride=2),
+            nn.ReLU6(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, stride=2),
+            nn.ReLU6(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU6(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+        )
+        self.convBlock2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=1),
+            nn.ReLU6(inplace=True),
+            nn.Conv2d(64, 192, kernel_size=3),
+            nn.BatchNorm2d(192),
+            nn.ReLU6(inplace=True),
+            nn.MaxPool2d(3, stride=2)
+        )
+        self.WIMCNNUnit = nn.Sequential(
+            WIMCNNBlock(64, (96, 128), (16, 24, 32), 32),
+            WIMCNNBlock(128, (128, 192), (32, 64, 96), 64),
+            WIMCNNBlock(192, (96, 208), (16, 32, 48), 64),
+        )
+        self.AvgPool = nn.AvgPool2d(13)
+        self.linear = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        out = self.convBlock1(x)
+        out = self.convBlock2(out)
+        WIMCNNUnitOut = self.WIMCNNUnit(out)
+        out = out + WIMCNNUnitOut
+        out = self.AvgPool(out)
+        result = self.linear(out)
+
+        return result
