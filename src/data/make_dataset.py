a24c355fbc2f533b45c4b30539b1925c5ca535e4
Piotr Chojnowski <40302552+piotr107@users.noreply.github.com>
2023-04-23 14:25:36 +0200

    fix dataset dir

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -22,10 +22,10 @@ def get_data_loaders():
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ]),
     }
-    train_set = WoodTypesDataset(data_dir="../../data/external/wood_dataset/train", dataset_type="train",
-                                        data_transforms=data_transforms["train"])
-    val_set = WoodTypesDataset(data_dir="../../data/external/wood_dataset/val", dataset_type="val",
-                                      data_transforms=data_transforms["val"])
+    train_set = WoodTypesDataset(data_dir="../data/external/wood_dataset/train", dataset_type="train",
+                                 data_transforms=data_transforms["train"])
+    val_set = WoodTypesDataset(data_dir="../data/external/wood_dataset/val", dataset_type="val",
+                               data_transforms=data_transforms["val"])
     train_loader = data.DataLoader(train_set, batch_size=32, shuffle=True, num_workers=2)
     val_loader = data.DataLoader(val_set, batch_size=32, shuffle=False, num_workers=2)
     return train_loader, val_loader
--- src/data/models/wood_type_dataset.py
+++ src/data/models/wood_type_dataset.py
@@ -10,7 +10,6 @@ from torchvision import datasets
 class WoodTypesDataset(torch.utils.data.Dataset):
     def __init__(self, data_dir, dataset_type, data_transforms=None):
         self.dataset_type = dataset_type
-        print("Current working directory: {0}".format(os.getcwd()))
         self.data = datasets.ImageFolder(os.path.join(data_dir), data_transforms)
         self.labels = self.data.targets
         self.transforms = data_transforms
